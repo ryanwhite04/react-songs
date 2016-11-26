b062d68fa9a82fd4c70330a7ef35de235ec50e44
Tom Pennycott <thomasp@rent.com.au>
2016-11-26 16:33:30 +0800

    Implement basic synth

--- src/Player.js
+++ src/Player.js
@@ -17,22 +17,18 @@ export default class Player extends Component {
   render = () =>
     <Song playing={this.state.playing} tempo={90} >
       <Analyser onAudioProcess={this.handleAudioProcess}>
-        {/* <Sequencer resolution={16} bars={1} >
-          <Sampler sample="samples/kick.wav" steps={[0, 2, 8, 10]} />
-          <Sampler sample="samples/snare.wav" steps={[4, 12]} />
-        </Sequencer>
         <Sequencer resolution={16} bars={2} >
           <Synth
             type="sine"
             steps={[
-              [0, 8, 'c2'],
-              [8, 4, 'c2'],
-              [12, 4, 'd#2'],
-              [16, 8, 'f2'],
-              [24, 8, 'f1'],
+              [0, 8, 'c4'],
+              [8, 4, 'c4'],
+              [12, 4, 'd#4'],
+              [16, 8, 'f4'],
+              [24, 8, 'f4'],
             ]}
           />
-        </Sequencer> */}
+        </Sequencer>
       </Analyser>
     </Song>
 }
